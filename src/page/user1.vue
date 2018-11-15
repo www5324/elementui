85e524e94170d63332f7d3fad9130ea00999ee02
www5324 <www5324@163.com>
2018-11-15 09:10:24 +0800

    234234

--- src/page/user1.vue
+++ src/page/user1.vue
@@ -1,5 +1,43 @@
 <template>
     <div>
         用户管理1
+        <a href="javascript:" @click="mast()">点击</a>
+        <a href="javscript:" @click="mast2()">点击2</a>
     </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+    name:'index',
+    data(){
+      return{
+        fdata:'',
+        vdata:'111',
+        dataurl:"userapi/findMailList"
+      }
+    },
+    methods:{
+            mast(){
+                axios.post("/apis/api/userapi/findMailList ",function(res){
+                    console.log(res);
+                } )
+              .then(function (response) {
+                console.log(response);
+              })
+        },
+        mast2()
+        {
+             axios.post("/api/post4",function(res){
+                    console.log(res);
+                } )
+              .then(function (response) {
+                console.log(response);
+              })
+        }
+
+    },
+    
+
+}
+</script>
+
